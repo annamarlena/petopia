48f8866247b29d0586c2a41f65e3eacedb9fe212
Mylan <moe1401@gmail.com>
2023-05-14 14:46:38 -0500

    added and updated Data files

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,7 +2,7 @@ const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const profileData = require('./projectData.json'); //needs to be changed still to match other data and tables
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,9 +12,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const profile of profileData) {
+    await profile.create({
+      ...profile,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
